5b619d95a5f81fd769ea0a20de2504ace15c0751
nikhillondhe9 <nikhillondhe9@gmail.com>
2022-08-04 22:17:32 -0400

    Angular Skeleton

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'The Dating app';
+  title = 'The Dating App';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient){}
   ngOnInit() {
     this.getUsers();
   }
@@ -19,6 +19,6 @@ export class AppComponent implements OnInit {
     this.http.get('https://localhost:5001/api/users').subscribe({
       next: response => this.users = response,
       error: error => console.log(error)
-    });
+    })
   }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
